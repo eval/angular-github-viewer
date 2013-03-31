600d12273c93aead9aece5ad7f707f51151ce8f9
Gert Goet <gert@thinkcreate.nl>
2013-03-31 21:26:17 +0200

    Showing commits

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,10 +6,24 @@
 function AppCtrl($scope, $http, $log) {
   $scope.members = [];
 
-  $scope.action = function(organization){
+  $scope.getMembers = function(organization){
     $log.log("Wanna know about " + organization + ", huh?");
     $http.get('json/members.json').success(function(data){
       $scope.members = data.data;
+    });
+  }
+
+  $scope.getRepos = function(member){
+    $log.log("Wanna get repos from " + member + ", huh?");
+    $http.get('json/repos.json').success(function(data){
+      $scope.repos = data.data;
+    });
+  }
+
+  $scope.getCommits = function(repo){
+    $log.log("Wanna get commits from " + repo + ", huh?");
+    $http.get('json/commits.json').success(function(data){
+      $scope.commits = data.data;
     });
   }
 }
